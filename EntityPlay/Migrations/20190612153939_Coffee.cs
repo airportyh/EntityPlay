f90da783a1b7300b19f681ca59927e2eb0c775b3
Toby <toby.ho@eyqahub.com>
2019-06-12 11:40:10 -0400

    coffee

---
+++ EntityPlay/Migrations/20190612153939_Coffee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityPlay.Migrations
+{
+    public partial class Coffee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Appointments",
+                columns: new[] { "Id", "CustomerId", "Date", "Title" },
+                values: new object[] { 4, 2, new DateTimeOffset(new DateTime(2019, 6, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -4, 0, 0, 0)), "Get coffee" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
